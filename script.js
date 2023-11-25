6a093805a14db20919bf8722c8778b6267eb3d1a
SwapnilMahmud24 <151989914+SwapnilMahmud24@users.noreply.github.com>
2023-11-25 13:25:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ocument.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // Add logic for handling login here
+    });
+});
